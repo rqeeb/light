b5b251b6c5a071007ffa38ffae1f18f66949ee10
RaqeebDev <raqeebauto@gmail.com>
2026-03-14 16:01:52 +0530

    installed tailwind/daisy ui and initted router | components and pages folder init

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,10 @@
-import { useState } from "react";
-
 function App() {
   return (
     <div>
-      {/* Zonnect - A chat app. [WIP] */}
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+     <button className = "btn btn-primary"> Button</button>
     </div>
   );
+
 }
 
 export default App;
