fc3092892d47b30a847e81640cb690a90dfd9b9d
RaqeebDev <raqeebauto@gmail.com>
2026-03-24 16:24:09 +0530

    ChatPage: difined imports and structure

--- frontend/src/pages/ChatPage.jsx
+++ frontend/src/pages/ChatPage.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useAuthStore } from "../store/useAuthStore";
+import { useChatStore } from "../store/useAuthStore";
 import BorderAnimatedContainer from "../components/borderAnimatedContainer";
 
 const ChatPage = () => {
@@ -7,9 +7,14 @@ const ChatPage = () => {
   return (
     <div className="relative w-full max-w-6xl h-[800px]">
       <BorderAnimatedContainer>
+
+        {/* Left SIDE */}
         <div className="w-80 bg-slate-800/50 backdrop-blur-sm flex flex-col ">
           <ProfileHeader />
           <ActiveTabSwitch />
+          <div className= "flex-1 overflow-y-auto p-4 space-y-2 ">
+          {activeTab === "chats" ? <ChatsList/> : <ContactList/>}
+          </div>
         </div>
       </BorderAnimatedContainer>
     </div>
