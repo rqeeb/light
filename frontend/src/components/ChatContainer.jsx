2abae2e66efc8be77d46c01a345e3c4c387b58b5
RaqeebDev <raqeebauto@gmail.com>
2026-04-02 15:46:26 +0530

    typo

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -72,7 +72,7 @@ function ChatContainer() {
               </div>
             ))}
 
-            <div ref={} />
+            <div ref={messageEndRef} />
 
           </div>
         ) : isMessagesLoading ? (
