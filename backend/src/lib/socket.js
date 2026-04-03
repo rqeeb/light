96448574470adb23ff1a4b976ff5d88178c5f496
RaqeebDev <raqeebauto@gmail.com>
2026-04-03 17:04:38 +0530

    imports socket

---
+++ backend/src/lib/socket.js
@@ -0,0 +1,14 @@
+import { Server } from "socket.io";
+import http from "http";
+import express from "express";
+import { ENV } from "./env.js";
+
+const app = express();
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    orgin: [ENV.CLIENT_URL],
+    credentials: true,
+  },
+});
