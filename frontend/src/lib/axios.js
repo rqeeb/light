bbad156f691635383118626363e2a4e44dcf486f
RaqeebDev <raqeebauto@gmail.com>
2026-04-06 13:13:12 +0530

    ahhh deployemtn bug

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -4,6 +4,6 @@ export const axiosInstance = axios.create({
   baseURL:
     import.meta.env.MODE === "development"
       ? "http://localhost:2022/api"
-      : "https://zonnect.onrender.com/",
+      : "https://zonnect.onrender.com/api",
   withCredentials: true,
 });
